5bd68bb54dc7ad61561a6de5ccb813612d3e1472
Michele Murakami <mmurakam@ucsd.edu>
2020-02-27 10:23:28 -0800

    thirdparty API lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -10,13 +10,13 @@ $(document).ready(function() {
  */
 function initializePage() {
 	console.log("Page ready");
- 	// initCamera();
- 	// initMap();
+  initCamera();
+ 	initMap();
  	initGestures();
  	initRSVPForm();
 }
 
-// init jQuery gestures  
+// init jQuery gestures
 function initGestures() {
 	// add gestures listener here
 }
@@ -24,4 +24,18 @@ function initGestures() {
 // init RSVP form submit listener
 function initRSVPForm() {
   // add your code here
+  $('#rsvpForm').submit(function(e) {
+
+    //prevent default submit + reload(we only want to submit part)
+    e.preventDefault();
+    console.log("submitting form...");
+    var rsvpEmail = $('#rsvpEmail').val();
+    //send the post request
+    $.post('addRSVP', {rsvpEmail: rsvpEmail}, postCallback);
+  });
+
+  function postCallback(res) {
+    alert("RSVP form successfully submitted!");
+    $('#rsvpEmail').val('');//clear form
+  }
 }
--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,14 @@
 function initMap() {
 	// add your code here
+  L.mapquest.key = '49cSMX9PVfBwlGkZTwdDWHAdAvxWtJo0';
+
+// 'map' refers to a <div> element with the ID map
+var map = L.mapquest.map('map', {
+  center: [32.8755, -117.2323],
+  layers: L.mapquest.tileLayer('map'),
+  zoom: 12
+});
+
+//add marker to mapquest
+L.marker([32.8755, -117.2323]).addTo(map);
 }
